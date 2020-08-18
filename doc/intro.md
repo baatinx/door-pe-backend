fb9eaddad85d029533489758b9e495db3de53e23
Mustafa Basit <mailtobasit74@gmail.com>
2020-08-18 22:13:21 +0530

    initial commit - template via - lein new app doorpe.backend.server.core

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to doorpe.backend.server.core
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/doorpe/backend/server/core_test.clj
@@ -0,0 +1,7 @@
+(ns doorpe.backend.server.core-test
+  (:require [clojure.test :refer :all]
+            [doorpe.backend.server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
